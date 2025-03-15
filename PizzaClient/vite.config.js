759c53e736425ff61975a8535ba50ec52b9b5a33
David Leal Blanes <120286862+Wormito@users.noreply.github.com>
2025-03-15 21:51:51 +0000

    Agregar archivos de configuración y estructura inicial para el cliente de Pizza; eliminar configuraciones obsoletas.

---
+++ PizzaClient/vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,  // Client port
+    target: 'https://vigilant-spoon-9vj5pxw5jwwcvx9-5100.app.github.dev/', // Mock server URL
+    proxy: {
+      '/pizzas': {
+        target: '5100', // Mock server port
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        configure: (proxy) => {
+          proxy.on('error', (err) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
+      },
+    },
+  },
+})
